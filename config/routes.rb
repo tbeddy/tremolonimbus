a0b6627646e71dab23b3e02f11b86efec039acb5
Tim Bedford <tmbdfd@gmail.com>
2020-11-20 14:56:32 -0500

    Move some of the track routes out of the users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
+    resources :tracks, only: [:index, :create]
     resources :users, only: [:create] do
-      resources :tracks, only: [:index, :show, :create]
+      resources :tracks, only: [:show]
     end
     resource :session, only: [:create, :destroy]
   end
