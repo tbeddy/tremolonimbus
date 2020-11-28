66a4bde4aec966f01533efb534b37130fa8cf694
Tim Bedford <tmbdfd@gmail.com>
2020-11-28 18:02:41 -0500

    Add track description to db

---
+++ db/migrate/20201128230029_add_description_column_to_tracks.rb
@@ -0,0 +1,5 @@
+class AddDescriptionColumnToTracks < ActiveRecord::Migration[5.2]
+  def change
+    add_column :tracks, :description, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_215259) do
+ActiveRecord::Schema.define(version: 2020_11_28_230029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2020_11_18_215259) do
     t.integer "play_count", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
   create_table "users", force: :cascade do |t|
