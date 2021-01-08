8e7501a21606d0ac292544766429eba08fd81e04
Tim Bedford <tmbdfd@gmail.com>
2021-01-08 15:19:52 -0500

    Add gradient picture to page player

--- frontend/components/player/player.jsx
+++ frontend/components/player/player.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { toMinutesAndSeconds } from '../../util/player_util';
+import { generateProfilePicture } from '../../util/pic_util';
 
 class Player extends React.Component {
   constructor(props) {
@@ -114,59 +115,65 @@ class Player extends React.Component {
     );
     return (
       <div className="page-player">
-        <div className="track-info-and-button">
-          <button
-            className="play-pause-button"
-            onClick={this.playOrPause}
-          >
-            <img
-              src={this.props.playing ? window.pauseWhiteURL : window.playWhiteURL}
-            />
-          </button>
-          <div>
-            <p className="artist-name">
-              <Link to={`/users/${this.props.uploader.id}`}>
-                {this.props.uploader.username}
-              </Link>
-            </p>
-            <p className="track-name">
-              <Link to={`/tracks/${this.props.id}`}>
-                {this.props.title}
-              </Link>
-            </p>
-          </div>
-        </div>
         <div
-          className="grey-bar"
-          onClick={this.seekAudio}
-          onMouseMove={this.changeSeekPosition}
-        >
-          <div
-            className="orange-bar"
-            style={{ width: `${this.state.percentDone}%` }}
-          ></div>
-          <div className="track-times">
-            {currentTime}
-            {/* <div className="duration">
-              {toMinutesAndSeconds(this.state.duration)}
-            </div> */}
+          className="page-player-picture"
+          style={{ "backgroundImage": generateProfilePicture(this.props.uploader.id) }}
+        />
+        <div className="page-player-except-picture">
+          <div className="track-info-and-button">
+            <button
+              className="play-pause-button"
+              onClick={this.playOrPause}
+            >
+              <img
+                src={this.props.playing ? window.pauseWhiteURL : window.playWhiteURL}
+              />
+            </button>
+            <div>
+              <p className="artist-name">
+                <Link to={`/users/${this.props.uploader.id}`}>
+                  {this.props.uploader.username}
+                </Link>
+              </p>
+              <p className="track-name">
+                <Link to={`/tracks/${this.props.id}`}>
+                  {this.props.title}
+                </Link>
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="track-buttons">
-          {deleteButton}
-          <div className="track-data">
-            {this.props.play_count === 0 ? null : (
-              <div className="play-count">
-                <img src={window.playGreyURL} />
-                {this.props.play_count}
-              </div>
-            )}
-            {this.props.comments.length === 0 ? null : (
-              <div className="comment-count">
-                <img src={window.chatURL} />
-                {this.props.comments.length}
-              </div>
-            )}
+          <div
+            className="grey-bar"
+            onClick={this.seekAudio}
+            onMouseMove={this.changeSeekPosition}
+          >
+            <div
+              className="orange-bar"
+              style={{ width: `${this.state.percentDone}%` }}
+            ></div>
+            <div className="track-times">
+              {currentTime}
+              {/* <div className="duration">
+                {toMinutesAndSeconds(this.state.duration)}
+              </div> */}
+            </div>
+          </div>
+          <div className="track-buttons">
+            {deleteButton}
+            <div className="track-data">
+              {this.props.play_count === 0 ? null : (
+                <div className="play-count">
+                  <img src={window.playGreyURL} />
+                  {this.props.play_count}
+                </div>
+              )}
+              {this.props.comments.length === 0 ? null : (
+                <div className="comment-count">
+                  <img src={window.chatURL} />
+                  {this.props.comments.length}
+                </div>
+              )}
+            </div>
           </div>
         </div>
       </div>
