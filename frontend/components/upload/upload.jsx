92938c080edd34eac6e399cb4a59957663bac79f
Tim Bedford <tmbdfd@gmail.com>
2021-01-11 17:34:08 -0500

    Refactor upload into a functional component

--- frontend/components/upload/upload.jsx
+++ frontend/components/upload/upload.jsx
@@ -1,65 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TrackForm from './track_form';
 
-class Upload extends React.Component {
-  constructor(props) {
-    super(props);
+export default props => {
+  const [file, setFile] = useState(null);
 
-    this.state = {
-      file: null
-    }
+  const uploadControls = (
+    <div className="upload-controls">
+      <p className="upload-message">
+        Upload your track here
+      </p>
+      <div className="upload-button">
+        <label
+          className="input-label"
+          htmlFor="upload"
+        >choose the file to upload</label>
+        <input
+          id="upload"
+          type="file"
+          className="hidden-input"
+          onChange={e => setFile(e.currentTarget.files[0])}
+        />
+      </div>
+    </div>
+  );
 
-    this.handleFile = this.handleFile.bind(this);
-    this.forgetFile = this.forgetFile.bind(this);
-  }
-
-  handleFile(e) {
-    this.setState({
-      file: e.currentTarget.files[0]
-    });
-  }
-
-  forgetFile() {
-    this.setState({
-      file: null
-    });
-  }
-
-  render() {
-    const { file } = this.state;
-    const uploadControls = (
-      <div className="upload-controls">
-        <p className="upload-message">
-          Upload your track here
-        </p>
-        <div className="upload-button">
-          <label
-            className="input-label"
-            htmlFor="upload"
-          >choose the file to upload</label>
-          <input
-            id="upload"
-            type="file"
-            className="hidden-input"
-            onChange={this.handleFile}
+  return (
+    <div className="upload-page">
+      <div className="upload-box">
+        {file === null ? uploadControls :
+          <TrackForm
+            {...props}
+            file={file} title={""} description={""}
+            forgetFile={() => setFile(null)}
           />
-        </div>
+        }
       </div>
-    );
-    return (
-      <div className="upload-page">
-        <div className="upload-box">
-          {this.state.file === null ? uploadControls :
-            <TrackForm
-              {...this.props} file={file}
-              title={""} description={""}
-              forgetFile={this.forgetFile}
-            />
-          }
-        </div>
-      </div>
-    )
-  }
+    </div>
+  )
 }
-
-export default Upload;
